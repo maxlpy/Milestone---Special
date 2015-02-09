bcc6cbf801468b9e5c6fa129fceb008b30f45e50
maxlpy <maxlipengyu@gmail.com>
2015-02-09 00:29:03 -0500

    add code into test

--- src/main/java/com/fred/webapp/MavenProject/Test.java
+++ src/main/java/com/fred/webapp/MavenProject/Test.java
@@ -2,7 +2,8 @@ package com.fred.webapp.MavenProject;
 
 public class Test {
 	public static void main(String[] args) {
-		String str = "Hello World.";
-		System.err.println(str);
+		String str = "Hello";
+		String str1 = "World.";
+		System.err.println(str + str1);
 	}
 }
