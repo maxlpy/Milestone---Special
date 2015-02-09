a31476a1546c6ac3be7c3aa7c390e644f70b08fa
maxlpy <maxlipengyu@gmail.com>
2015-02-09 12:08:16 -0500

    test

--- src/main/java/com/fred/webapp/MavenProject/Test.java
+++ src/main/java/com/fred/webapp/MavenProject/Test.java
@@ -3,7 +3,6 @@ package com.fred.webapp.MavenProject;
 public class Test {
 	public static void main(String[] args) {
 		String str = "Hello ";
-		String str1 = "World!";
-		System.out.println(str + str1);
+		System.out.println(str);
 	}
 }
