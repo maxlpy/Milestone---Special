464e342998754f39d259da0fc08d56ca30f80d81
Nikhil Katre <nkatre@ncsu.edu>
2015-04-22 23:37:22 -0400

    add

--- pushToProduction.sh
+++ pushToProduction.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 git config --global user.name "Nikhil Katre"
 git config --global user.email "nkatre@ncsu.edu"
+git pull
 git add .
 git commit -m "committed the project for production release"
+git push origin master
 git push production master
 #
