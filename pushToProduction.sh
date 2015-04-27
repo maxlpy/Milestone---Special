e6ffdce1e4aa9a73bb4008534e7ca71adcd6f1d8
Nikhil Katre <nkatre@ncsu.edu>
2015-04-27 13:23:43 -0400

    added

--- pushToProduction.sh
+++ pushToProduction.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#ssh -t 127.0.0.1 "sudo command"
 sudo git config --global user.name "Nikhil Katre"
 sudo git config --global user.email "nkatre@ncsu.edu"
-#sudo git add -A
-#sudo git commit -m "add"
 sudo git pull origin master
 sudo git push production master
-#
